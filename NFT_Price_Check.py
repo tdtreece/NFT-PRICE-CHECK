1c9bb4a76e388155a71686d56345789b8ba7f7c2
Taylor Treece <67754580+taylor-d-treece@users.noreply.github.com>
2022-02-08 23:28:00 -0600

    Update NFT_Price_Check.py

--- NFT_Price_Check.py
+++ NFT_Price_Check.py
@@ -28,7 +28,7 @@ async def nft_buys(message):
         nft_floor += str('{} | {} ETH \n'.format(nft_list[i].call_date, nft_list[i].floor))
         nft_price += str('{} ETH \n'.format(nft_list[i].price))
 
-    embed_nft = discord.Embed(title="NFT Buys", url='https://opensea.io/', description="<#912536067071172618>", color=0x63b3f7)
+    embed_nft = discord.Embed(title="OpenSea NFTs", url='https://opensea.io/', description="<#912536067071172618>", color=0x63b3f7)
     embed_nft.set_thumbnail(url='https://storage.googleapis.com/opensea-static/Logomark/Logomark-Blue.png')
     embed_nft.add_field(name='__NFT Collection__', value=nft_message, inline=True)
     embed_nft.add_field(name='__Call Date | Call Price__', value=nft_floor, inline=True)
