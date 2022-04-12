e7334e17c5c5443c5ef5606c2d5dbd7d4ed4ac59
Taylor Treece <67754580+taylor-d-treece@users.noreply.github.com>
2022-04-11 21:57:31 -0500

    Update NFT_Class.py

--- NFT_Class.py
+++ NFT_Class.py
@@ -5,12 +5,11 @@ nft_list = []
 
 class Nft:
 
-    def __init__(self, name, floor, api, call_date):
+    def __init__(self, name, floor, api):
         self.name = name
         self.floor = floor
         self.api = api
         self.url = "https://api.opensea.io/api/v1/collection/{}/stats".format(api)
-        self.call_date = call_date
         nft_list.append(self)
 
     def nft_price_check(self):
