36459ec6cd437653b4bef307ce4c6dc1b5b92668
Taylor Treece <67754580+taylor-d-treece@users.noreply.github.com>
2022-04-11 22:07:19 -0500

    Update NFT_Class.py

--- NFT_Class.py
+++ NFT_Class.py
@@ -5,9 +5,8 @@ nft_list = []
 
 class Nft:
 
-    def __init__(self, name, floor, api):
+    def __init__(self, name, api):
         self.name = name
-        self.floor = floor
         self.api = api
         self.url = "https://api.opensea.io/api/v1/collection/{}/stats".format(api)
         nft_list.append(self)
@@ -17,3 +16,9 @@ class Nft:
         data = r.json()
         self.price = data['stats']['floor_price']
         # print('The current floor price of {} is {} ETH.'.format(self.name, str(self.price)))
+
+# Create NFT Class Object Here -- See Example Below
+# This will create the NFT object and add it to the list that will message your Discord server
+# You will get the API from the Opensea.io URL for the collection
+# https://opensea.io/collection/cryptopunks
+CryptoPunks = Nft(name='CryptoPunks', api='cryptopunks')
